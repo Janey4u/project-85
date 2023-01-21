f9fefe387731dece42fdf5cbf8146682d6e94750
Janey4u <106005467+Janey4u@users.noreply.github.com>
2023-01-21 10:01:28 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+
+
+
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+
+
+function add()
+moveTo= "the left";
+	
+
+
+function uploadBackground() 
+
+background_image = " cars"
+
+function uploadgreencar() {
+	
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	
+}
+
+function down()
+{
+	
+}
+
+function left()
+{
+	
+}
+
+function right()
+{
+	
+}
